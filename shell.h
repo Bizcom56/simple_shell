1961cfa0e82deb0e619bcaef94de4d63fd67f8f6
“Bizcom56” <dornorjoshua56@gmail.com>
2023-12-15 17:52:11 -0800

    new updates

--- shell.h
+++ shell.h
@@ -11,16 +11,33 @@
 #include <fcntl.h>
 #include <signal.h>
 #include <limits.h>
+#include <string.h>
 
 #define BUFSIZE 1024
 #define TOK_BUFSIZE 128
 #define TOK_DELIM " \t\r\n\a"
 
-/* envrn1.c  */
+/* cd_shell.c */
+int _strcmp(const char *s1, const char *s2);
+char *_strdup(const char *s);
+void set_env(const char *name, const char *value, char ***environ);
+void rev_string(char *str);
+char *_strtok(char *str, const char *delim);
+void get_error(int *status, int code);
+size_t _strlen(const char *s);
+
+int cd_shell(char **args, char ***environ, int *status);
+void cd_dot(char ***environ, int *status);
+void cd_to(char *dir, char ***environ, int *status);
+void cd_previous(char ***environ, int *status);
+void cd_to_home(char ***environ, int *status);
+
+
+/* sh_env.c  */
 char *_getenv(const char *name);
 int _env(void);
 
-/* Points to an array of pointers to strings called the "environment" */
+/* Function prototypes */
 extern char **environ;
 char **token(char *buffer);
 void free_arg(char **argv);
